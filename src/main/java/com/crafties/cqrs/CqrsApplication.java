30aa85cf20977e6ea87f88f24fa8cea387b4e4ff
Nadia Humbert-Labeaumaz <np.humbert@gmail.com>
2016-08-07 20:11:24 +0200

    feature(*): Initial commit

---
+++ src/main/java/com/crafties/cqrs/CqrsApplication.java
@@ -0,0 +1,12 @@
+package com.crafties.cqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CqrsApplication.class, args);
+	}
+}
